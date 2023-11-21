0329425113ebf99853b65be53a6f03e1dfb75829
Al Stone <astone@heckgrammar.co.uk>
2023-11-21 15:27:31 +0000

    D4WorkInProgress

--- AdventOfCode2018/Program.cs
+++ AdventOfCode2018/Program.cs
@@ -1,5 +1,9 @@
-﻿using System.Collections.Concurrent;
+﻿using Microsoft.Win32.SafeHandles;
+using System.Collections.Concurrent;
 using System.ComponentModel;
+using System.IO.IsolatedStorage;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
 
 namespace AdventOfCode2018
 {
@@ -7,7 +11,7 @@ namespace AdventOfCode2018
     {
         static void Main(string[] args)
         {
-            Day3();
+            Day4();
         }
         static void Day1()
         {
@@ -139,9 +143,9 @@ namespace AdventOfCode2018
         }
         static void Day3()
         {
-           StreamReader sr = new StreamReader("day3input.txt");
-           
-           // StreamReader sr = new StreamReader("test.txt");
+            StreamReader sr = new StreamReader("day3input.txt");
+
+            // StreamReader sr = new StreamReader("test.txt");
             Dictionary<(int x, int y), int> grid = new Dictionary<(int x, int y), int>();
 
             Dictionary<(int x, int y), List<int>> claims = new Dictionary<(int x, int y), List<int>>();
@@ -159,14 +163,15 @@ namespace AdventOfCode2018
                 int y = int.Parse(coords[1]);
                 var cord = (x, y);
                 string[] size = lSplit[3].Split("x");
-               
+
                 int w = int.Parse(size[0]);
                 int h = int.Parse(size[1]);
 
-                for(int i = x;i< x + w; i++){
-                    for (int j = y;j< y + h; j++)
+                for (int i = x; i < x + w; i++)
+                {
+                    for (int j = y; j < y + h; j++)
                     {
-                        
+
                         //Console.WriteLine($"{i}x{j}");
                         //Console.ReadLine();
                         cord = (i, j);
@@ -174,7 +179,7 @@ namespace AdventOfCode2018
                         {
                             claims[cord].Add(claim);
                             ClaimNumbers.Remove(claim);
-                            for(int c = 0; c < claims[cord].Count; c++)
+                            for (int c = 0; c < claims[cord].Count; c++)
                             {
                                 ClaimNumbers.Remove(claims[cord][c]);
                             }
@@ -188,7 +193,7 @@ namespace AdventOfCode2018
                         if (grid.ContainsKey(cord))
                         {
                             grid[cord]++;
-                      
+
                         }
                         else
                         {
@@ -196,17 +201,141 @@ namespace AdventOfCode2018
                         }
                     }
                 }
-               
+
 
             }
             int overlaps = 0;
-     for (int i = 0; i<ClaimNumbers.Count; i++)
+            for (int i = 0; i < ClaimNumbers.Count; i++)
             {
                 Console.WriteLine(ClaimNumbers[i]);
             }
-           
-        }
 
+        }
+        static void Day4()
+        {
+           StreamReader sr = new StreamReader("test.txt");
+            //  StreamReader sr = new StreamReader("day4Input.txt");
+            int startTime = 0;
+            int endTime = 0;
+            Dictionary<DateTime, string> inputs = new Dictionary<DateTime, string>();
+            Dictionary<int, int[]> guard = new Dictionary<int, int[]>();
+            while (!sr.EndOfStream)
+            {
+
+                string line = sr.ReadLine();
+                DateTime d = Convert.ToDateTime(line.Substring(1, 16));
+                string details = line.Substring(19);
+                inputs.Add(d, details);
+                //   Console.WriteLine($"Date and time = {d} deta {details}");
+
+
+            }
+            List<DateTime> datesandTimes = inputs.Keys.ToList();
+            datesandTimes.Sort();
+            Dictionary<string, Dictionary<int, int[]>> data = new Dictionary<string, Dictionary<int, int[]>>();
+            Dictionary<int, int[]> guardData = new Dictionary<int, int[]>();
+            int guardNumber = -1;
+            int numCount = 0;
+            foreach (DateTime key in datesandTimes)
+            {
+               
+                //Console.WriteLine($"{key} {inputs[key]}");
+                string d = key.ToString("yyyy-MM-dd");
+                string details = inputs[key];
+                Console.WriteLine(details);
+
+                if (details.Contains("Guard"))
+                {
+                    string numbers = "[^0-9]";
+                    string guardNum = Regex.Replace(details, numbers, "");
+                    guardNumber = Convert.ToInt32(guardNum);
+                    if(!guardData.ContainsKey(guardNumber))
+                    {
+                        guardData.Add(guardNumber, new int[60]);
+                    }
+                  
+                }
+                else
+                {
+                    if (details.Contains("falls"))
+                    {
+                      
+                        startTime = key.Minute;
+                        numCount++;
+                    }
+                    if (details.Contains("wakes"))
+                    {
+                        endTime = key.Minute;
+                        numCount++;
+                    }
+                    if (numCount == 2)
+                    {
+                         for (int i = startTime; i < endTime; i++)
+                        {
+                            guardData[guardNumber][i]++;
+                        }
+                        numCount = 0;
+                    }
+
+
+
+                }
+            }
+            string ts = "";
+            string us = "";
+            for(int t = 0;t<6;t++) {
+                
+                   
+                    for (int u = 0;u < 10; u++)
+                    {
+                    ts += t.ToString();
+                    us += u.ToString();
+                    }
+                }
+                string st = "";
+                Console.WriteLine(st.PadRight(11) + ts);
+                Console.WriteLine(st.PadRight(11) + us);
+            foreach (KeyValuePair < int,int[]> gd in guardData)
+            {
+                int gsum = 0;
+                Console.Write($"{gd.Key.ToString().PadRight(10)} ");
+                for (int i = 0;i<60; i++)
+                {
+                    Console.Write($"{gd.Value[i]}");
+                    gsum++;
+                }
+          Console.WriteLine();
+                Console.WriteLine(gsum) ;
+                Console.WriteLine(gd.Value.Max());
+            }
+            
+        }
 
     }
 }
+
+//if (details.Contains("Guard"))
+//{
+//    string numbers = "[^0-9]";
+//    string guard = Regex.Replace(details, numbers, "");
+//    Console.WriteLine(guard);
+
+//}
+//else
+//{
+//    if (details.Contains("falls"))
+//    {
+//        startTime = d.Minute;
+//    }
+//    else
+//    {
+//        endTime = d.Minute;
+//    }
+//}
+
+//            }
+//            for (int i = startTime; i < endTime; i++)
+//{
+//    Console.Write(i.ToString() + " ");
+//}
+//Console.WriteLine();
